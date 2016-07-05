41b0e7f2d8e2deef16746feab85a25fe5b1cc93e
Robert Yuen <dungeonry@hotmail.com>
2016-07-05 11:45:00 -0700

    Added kijiji data generator.

--- kijijigenerate.js
+++ kijijigenerate.js
@@ -1,14 +1,66 @@
 var request = require("request");
 var cheerio = require("cheerio");
+var mongoose = require("mongoose");
+var config = require("./nconf.js");
+var RoomListing = require("./models/roomlisting.js");
 
-var url = "http://www.kijiji.ca/b-room-rental-roommate/vancouver/c36l1700287?ad=offering";
+var baseKijijiUrl = "http://www.kijiji.ca/b-room-rental-roommate/vancouver/c36l1700287?ad=offering";
 
-request(url, function(err, status, data){
+
+mongoose.connect(config.db, function(err){
+  if(err)
+    console.log(err);
+  console.log("Mongoose conn attempted.");
+});
+mongoose.connection.on("connected", function(){
+  console.log("Mongoose connection open to %s", config.db);
+});
+
+request(baseKijijiUrl, function(err, status, data){
   if(err)
     throw err;
   var $ = cheerio.load(data);
-  $("a.title.enable-search-navigation-flag").each(function(){
-    console.log($(this).text());
+
+  $(".info-container").each(function(){
+    var currRoomListing = new RoomListing();
+
+     currRoomListing.title = $(this).find(".title a").text().trim();
+     currRoomListing.price = $(this).find(".price").text().trim();
+     currRoomListing.area = $(this).find(".location").clone().children().remove().end().text().trim();     //Often just Vancouver
+     currRoomListing.date = $(this).find(".date-posted").text().trim();
+     currRoomListing.url = $(this).find(".title a").attr("href").trim();
+     currRoomListing.desc = $(this).find(".description").text().trim();
+
+    //Figure out how to make an async call on a value that isn't initialized at runtime
+    //  Something to do with require("child_process").exec;
+    //  request(currRoomListing.url, function(subErr, status, subData){
+    //    if(subErr){throw subErr}
+    //     var jquery = cheerio.load(subData);
+    //     console.log(jquery);
+    //  });
+
+
+    //TODO:
+    //  Drill Deeper for more info-container
+    //  Don't Save Doubles
+    //
+
+    RoomListing.find({"title": currRoomListing.title}, function(err, data){
+      if(err)
+        throw err;
+      //If it exists don't save, and skip
+
+      if(title == data.title){
+        console.log("RoomListing exists.");
+      }else{
+        currRoomListing.save(function(err, data){
+          if(err)
+            throw err;
+          console.log("Kijiji Room Listing Saved.\n %s", data);
+        });
+      }
+    });
+
+
   });
-  //console.log(data);
 });
--- models/roomlisting.js
+++ models/roomlisting.js
@@ -6,7 +6,14 @@ var roomListingSchema = mongoose.Schema({
   roomDim: String,
   area: String,
   date: String,
-  url: String
+  url: String,
+  desc: {type: String, required: false},
+  detailedDesk: {type: String, required: false},
+  address: {type: String, required: false},
+  furnished: {type: String, required: false},
+  pets: {type: String, required: false}
+
+
 }, {timestamps: {
   createdAt: "created_at"
 }});
